89a5aff620f8021e9e44f3be80b380dc4de4a62d
Maike Silva <mikemendessilv@gmail.com>
2023-03-13 16:31:16 -0400

    Leitura das teclas + Mostrar erro na calc
    Por algum motivo, o evento input não está sendo acionado quando o input da tela é modificado. Descobrir o porquê.

---
+++ script.js
@@ -0,0 +1,73 @@
+let teclas = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'Backspace', 'Delete', '+', '-', '/', '*', '=', '.', 'Enter']
+let tela = window.document.querySelector('input#tela')
+
+tela.addEventListener('input', function(){
+    if(tela.value == 'NaN' || tela.value == 'Infinity'){
+        tela.value = 'ERROR'
+    }
+})
+
+document.addEventListener('keydown', function(event){
+    console.log(event.key)
+    if(teclas.includes(event.key)){
+        switch (event.key){
+            case '1':
+                tela.value += 1
+                break
+            case '2':
+                tela.value += 2
+                break
+            case '3':
+                tela.value += 3
+                break
+            case '4':
+                tela.value += 4
+                break
+            case '5':
+                tela.value += 5
+                break
+            case '6':
+                tela.value += 6
+                break
+            case '7':
+                tela.value += 7
+                break
+            case '8':
+                tela.value += 8
+                break
+            case '9':
+                tela.value += 9
+                break
+            case '0':
+                tela.value += 0
+                break
+            case 'Backspace':
+                tela.value = tela.value.slice(0, -1)
+                break
+            case 'Delete':
+                tela.value = ''
+                break
+            case '.':
+                tela.value += '.'
+                break
+            case '+':
+                tela.value += '+'
+                break
+            case '-':
+                tela.value += '-'
+                break
+            case '/':
+                tela.value += '/'
+                break
+            case '*':
+                tela.value += '*'
+                break
+            case 'Enter':
+                tela.value = eval(tela.value)
+                break 
+            case '=':
+                tela.value = eval(tela.value)
+                break
+        }
+    }
+})
